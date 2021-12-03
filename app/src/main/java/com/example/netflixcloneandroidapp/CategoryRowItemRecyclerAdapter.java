75c95a3ec46ce7fd13e925095d5bc21883d5a5ba
andrei <git1@email.com>
2021-12-03 08:52:53 +0100

    Binding data to CategoryRowItem Recycler View

--- app/src/main/java/com/example/netflixcloneandroidapp/CategoryRowItemRecyclerAdapter.java
+++ app/src/main/java/com/example/netflixcloneandroidapp/CategoryRowItemRecyclerAdapter.java
@@ -4,13 +4,24 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
 public class CategoryRowItemRecyclerAdapter extends RecyclerView.Adapter<CategoryRowItemRecyclerAdapter.CategoryRowItemRecyclerViewHolder>{
 
     Context context;
+    List<Movie> movieList;
+
+    public CategoryRowItemRecyclerAdapter(Context context, List<Movie> movieList) {
+        this.context = context;
+        this.movieList = movieList;
+    }
 
     @NonNull
     @Override
@@ -23,17 +34,26 @@ public class CategoryRowItemRecyclerAdapter extends RecyclerView.Adapter<Categor
 
     @Override
     public void onBindViewHolder(@NonNull CategoryRowItemRecyclerViewHolder holder, int position) {
+        Glide.with(holder.itemView.getContext())
+                .load("https://image.tmdb.org/t/p/w500" + movieList.get(position).getBackdrop_path())
+                .into(holder.itemImage);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return movieList.size();
     }
 
     public class CategoryRowItemRecyclerViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView itemImage;
+
         public CategoryRowItemRecyclerViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            itemImage = itemView.findViewById(R.id.item_image);
+
         }
     }
 }
