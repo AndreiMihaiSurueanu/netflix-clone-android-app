0f655da3657d55ff9bdca32fff7d3b811a2054e2
andrei <git1@email.com>
2021-12-03 08:33:07 +0100

    Added CategoryRowItemRecyclerViewHolder

--- app/src/main/java/com/example/netflixcloneandroidapp/CategoryRowItemRecyclerAdapter.java
+++ app/src/main/java/com/example/netflixcloneandroidapp/CategoryRowItemRecyclerAdapter.java
@@ -1,24 +1,31 @@
 package com.example.netflixcloneandroidapp;
 
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CategoryRowItemRecyclerAdapter extends RecyclerView.Adapter{
+public class CategoryRowItemRecyclerAdapter extends RecyclerView.Adapter<CategoryRowItemRecyclerAdapter.CategoryRowItemRecyclerViewHolder>{
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public CategoryRowItemRecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull CategoryRowItemRecyclerViewHolder holder, int position) {
 
     }
 
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    public class CategoryRowItemRecyclerViewHolder extends RecyclerView.ViewHolder {
+        public CategoryRowItemRecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
     }
 }
