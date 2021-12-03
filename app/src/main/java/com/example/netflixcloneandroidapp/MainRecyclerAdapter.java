2bba412c6bf631c7c67bf468fe694aaa3ef28efd
andrei <git1@email.com>
2021-12-03 08:02:46 +0100

    Inflating Layout
    * Adding implementation to onCreateViewHolder method inside the MainRecyclerAdapter class

--- app/src/main/java/com/example/netflixcloneandroidapp/MainRecyclerAdapter.java
+++ app/src/main/java/com/example/netflixcloneandroidapp/MainRecyclerAdapter.java
@@ -1,24 +1,47 @@
 package com.example.netflixcloneandroidapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MainRecyclerAdapter extends RecyclerView.Adapter{
+import java.util.List;
+
+public class MainRecyclerAdapter extends RecyclerView.Adapter<MainRecyclerAdapter.MainRecyclerViewHolder>{
+
+    Context context;
+    List<String> listOfCategories;
+
+    public MainRecyclerAdapter(Context context, List<String> listOfCategories) {
+        this.context = context;
+        this.listOfCategories = listOfCategories;
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public MainRecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.main_recycler_category_row_item, parent,false);
+        return new MainRecyclerViewHolder(view);
+
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MainRecyclerViewHolder holder, int position) {
 
     }
 
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    public class MainRecyclerViewHolder extends RecyclerView.ViewHolder {
+        public MainRecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
     }
 }
