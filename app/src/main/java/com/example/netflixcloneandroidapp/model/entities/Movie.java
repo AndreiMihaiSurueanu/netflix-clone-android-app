9c1e9f77a1c29da2df3a6e76f45d7c3245996fde
andrei <git1@email.com>
2021-12-04 08:01:02 +0100

    Added the Parcelable implementation to the Movie model entity class

--- app/src/main/java/com/example/netflixcloneandroidapp/model/entities/Movie.java
+++ app/src/main/java/com/example/netflixcloneandroidapp/model/entities/Movie.java
@@ -1,8 +1,11 @@
 package com.example.netflixcloneandroidapp.model.entities;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Arrays;
 
-public class Movie {
+public class Movie implements Parcelable {
 
     private final String poster_path;
     private final boolean adult;
@@ -35,6 +38,35 @@ public class Movie {
         this.video = video;
         this.vote_average = vote_average;
     }
+
+    protected Movie(Parcel in) {
+        poster_path = in.readString();
+        adult = in.readByte() != 0;
+        overview = in.readString();
+        release_date = in.readString();
+        genre_ids = in.createIntArray();
+        id = in.readInt();
+        original_title = in.readString();
+        original_language = in.readString();
+        title = in.readString();
+        backdrop_path = in.readString();
+        popularity = in.readFloat();
+        vote_count = in.readInt();
+        video = in.readByte() != 0;
+        vote_average = in.readFloat();
+    }
+
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 
     public String getPoster_path() {
         return poster_path;
@@ -90,6 +122,29 @@ public class Movie {
 
     public float getVote_average() {
         return vote_average;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(poster_path);
+        parcel.writeByte((byte) (adult ? 1 : 0));
+        parcel.writeString(overview);
+        parcel.writeString(release_date);
+        parcel.writeIntArray(genre_ids);
+        parcel.writeInt(id);
+        parcel.writeString(original_title);
+        parcel.writeString(original_language);
+        parcel.writeString(title);
+        parcel.writeString(backdrop_path);
+        parcel.writeFloat(popularity);
+        parcel.writeInt(vote_count);
+        parcel.writeByte((byte) (video ? 1 : 0));
+        parcel.writeFloat(vote_average);
     }
 
     @Override
