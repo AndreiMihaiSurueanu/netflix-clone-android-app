51633735c59daa2c06736ef1f1e048c73b6cf122
andrei <git1@email.com>
2021-12-04 13:29:06 +0100

    Added Executors in MainViewModel

--- app/src/main/java/com/example/netflixcloneandroidapp/viewmodel/MainViewModel.java
+++ app/src/main/java/com/example/netflixcloneandroidapp/viewmodel/MainViewModel.java
@@ -7,13 +7,17 @@ import com.example.netflixcloneandroidapp.model.MoviesRepository;
 import com.example.netflixcloneandroidapp.model.entities.Movie;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainViewModel extends ViewModel {
 
     private MoviesRepository moviesRepository;
+    private final ExecutorService executorService;
 
     public MainViewModel() {
         this.moviesRepository = MoviesRepository.getInstance();
+        executorService = Executors.newCachedThreadPool();
     }
 
     public LiveData<List<Movie>> getTrendingNow() {
