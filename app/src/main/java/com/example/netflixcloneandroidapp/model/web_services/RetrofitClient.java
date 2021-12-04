5950dafcb6a5966325cc648d7c5284a77dba2752
andrei <git1@email.com>
2021-12-04 07:50:25 +0100

    Added getTrendingNow method implementation
    * Added getTrendingNow for retrieving the list of trending movies

--- app/src/main/java/com/example/netflixcloneandroidapp/model/web_services/RetrofitClient.java
+++ app/src/main/java/com/example/netflixcloneandroidapp/model/web_services/RetrofitClient.java
@@ -1,9 +1,15 @@
 package com.example.netflixcloneandroidapp.model.web_services;
 
+import android.util.Log;
+
 import com.example.netflixcloneandroidapp.model.entities.Movie;
+import com.example.netflixcloneandroidapp.model.entities.TrendingNow;
 
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -27,5 +33,22 @@ public class RetrofitClient {
         return instance;
     }
 
+
+    public List<Movie> getTrendingNow() {
+
+        Call<TrendingNow> trendingNowCall = tmdbApiInterface.trendingNowCall(ApiConfig.API_KEY);
+        trendingNowCall.enqueue(new Callback<TrendingNow>() {
+            @Override
+            public void onResponse(Call<TrendingNow> call, Response<TrendingNow> response) {
+                trendingMoviesList = response.body().getMovies();
+            }
+
+            @Override
+            public void onFailure(Call<TrendingNow> call, Throwable t) {
+                Log.i("Retrofit", "Something went wrong :(");
+            }
+        });
+        return trendingMoviesList;
+    }
 
 }
