593d5ceb71b69a53b31afa9c80f8c4edf90f25ce
andrei <git1@email.com>
2021-12-04 13:36:35 +0100

    Added loadTrendingNow method in Retrofit Client
    * Changed getTrendingNow to loadTrendingNow method for background Tread operations
    * And changed from asyncronous to syncronous API call to TMDB

--- app/src/main/java/com/example/netflixcloneandroidapp/model/web_services/RetrofitClient.java
+++ app/src/main/java/com/example/netflixcloneandroidapp/model/web_services/RetrofitClient.java
@@ -1,14 +1,11 @@
 package com.example.netflixcloneandroidapp.model.web_services;
 
-import android.util.Log;
-
 import com.example.netflixcloneandroidapp.model.entities.Movie;
 import com.example.netflixcloneandroidapp.model.entities.TrendingNow;
 
+import java.io.IOException;
 import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -17,7 +14,6 @@ public class RetrofitClient {
 
     private static RetrofitClient instance;
     TmdbApiInterface tmdbApiInterface;
-    List<Movie> trendingMoviesList;
 
     RetrofitClient() {
         tmdbApiInterface = new Retrofit.Builder()
@@ -34,21 +30,11 @@ public class RetrofitClient {
     }
 
 
-    public List<Movie> getTrendingNow() {
+    public List<Movie> loadTrendingNow() throws IOException {
 
-        Call<TrendingNow> trendingNowCall = tmdbApiInterface.trendingNowCall(ApiConfig.API_KEY);
-        trendingNowCall.enqueue(new Callback<TrendingNow>() {
-            @Override
-            public void onResponse(Call<TrendingNow> call, Response<TrendingNow> response) {
-                trendingMoviesList = response.body().getMovies();
-            }
+        Response response = tmdbApiInterface.trendingNowCall(ApiConfig.API_KEY).execute();
+        return ((TrendingNow) response.body()).getMovies();
 
-            @Override
-            public void onFailure(Call<TrendingNow> call, Throwable t) {
-                Log.i("Retrofit", "Something went wrong :(");
-            }
-        });
-        return trendingMoviesList;
     }
 
 }
