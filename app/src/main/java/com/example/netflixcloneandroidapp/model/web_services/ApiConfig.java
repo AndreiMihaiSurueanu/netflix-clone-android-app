78e9aec0d7a1516e3c985b59511f3017de780cce
andrei <git1@email.com>
2021-12-04 07:40:50 +0100

    Created ApiConfig helper class for Retrofit Client

---
+++ app/src/main/java/com/example/netflixcloneandroidapp/model/web_services/ApiConfig.java
@@ -0,0 +1,8 @@
+package com.example.netflixcloneandroidapp.model.web_services;
+
+public class ApiConfig {
+
+    public static final String BASE_URL = "https://api.themoviedb.org/3/";
+
+    public static final String API_KEY = "52a18783ed514602a5facb15a0177e61";
+}
