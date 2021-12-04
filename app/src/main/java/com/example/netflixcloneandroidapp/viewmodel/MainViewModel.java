17bf511d7acacfcc69b9bcbcb4266aa052d5178e
andrei <git1@email.com>
2021-12-04 13:32:12 +0100

    Added loadTrendingNow method in MainViewModel
    * Added loadTrendingNow method for background Tread operations for retrieving the list of treniding movies

--- app/src/main/java/com/example/netflixcloneandroidapp/viewmodel/MainViewModel.java
+++ app/src/main/java/com/example/netflixcloneandroidapp/viewmodel/MainViewModel.java
@@ -6,9 +6,11 @@ import androidx.lifecycle.ViewModel;
 import com.example.netflixcloneandroidapp.model.MoviesRepository;
 import com.example.netflixcloneandroidapp.model.entities.Movie;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class MainViewModel extends ViewModel {
 
@@ -18,6 +20,19 @@ public class MainViewModel extends ViewModel {
     public MainViewModel() {
         this.moviesRepository = MoviesRepository.getInstance();
         executorService = Executors.newCachedThreadPool();
+    }
+
+    public void loadTrendingNow() {
+        final Future future = executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    moviesRepository.loadTrendingNow();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     public LiveData<List<Movie>> getTrendingNow() {
