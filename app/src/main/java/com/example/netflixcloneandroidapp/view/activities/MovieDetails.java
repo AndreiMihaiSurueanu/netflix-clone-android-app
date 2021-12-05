d0e615a4adc5bf2e7002f507b93e93afd1687513
andrei <git1@email.com>
2021-12-05 18:10:27 +0100

    Connected XML Views in MovieDetails Activity

--- app/src/main/java/com/example/netflixcloneandroidapp/view/activities/MovieDetails.java
+++ app/src/main/java/com/example/netflixcloneandroidapp/view/activities/MovieDetails.java
@@ -3,14 +3,26 @@ package com.example.netflixcloneandroidapp.view.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.netflixcloneandroidapp.R;
 
 public class MovieDetails extends AppCompatActivity {
 
+    ImageView moviePoster, movieBackdrop;
+    TextView movieTitle, movieDescription;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+
+        moviePoster = findViewById(R.id.movie_card_poster);
+        movieBackdrop = findViewById(R.id.movie_backdrop);
+        movieTitle = findViewById(R.id.movie_title);
+        movieDescription = findViewById(R.id.movie_description);
+
+    }
     }
 }
