72bce5210b700e695a4a70d856019c49af99d86e
andrei <git1@email.com>
2021-12-05 18:22:07 +0100

    Getting the data from the Intent and Displaying them in the Views

--- app/src/main/java/com/example/netflixcloneandroidapp/view/activities/MovieDetails.java
+++ app/src/main/java/com/example/netflixcloneandroidapp/view/activities/MovieDetails.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.netflixcloneandroidapp.R;
+import com.example.netflixcloneandroidapp.model.entities.Movie;
 
 public class MovieDetails extends AppCompatActivity {
 
@@ -23,6 +25,23 @@ public class MovieDetails extends AppCompatActivity {
         movieTitle = findViewById(R.id.movie_title);
         movieDescription = findViewById(R.id.movie_description);
 
+        GetDataFromIntent();
     }
+
+    private void GetDataFromIntent() {
+        if(getIntent().hasExtra("movie")){
+            Movie movieModel = getIntent().getParcelableExtra("movie");
+
+            Glide.with(this)
+                    .load("https://image.tmdb.org/t/p/original" + movieModel.getBackdrop_path())
+                    .into(movieBackdrop);
+            Glide.with(this)
+                    .load("https://image.tmdb.org/t/p/original" + movieModel.getPoster_path())
+                    .into(moviePoster);
+
+            movieTitle.setText(movieModel.getTitle());
+            movieDescription.setText(movieModel.getOverview());
+
+        }
     }
 }
